7ab5366153d274b51c8f6e97ff0680ce93395f8b
Fredwuz <frederic23.mai@gmail.com>
2020-09-27 12:57:48 +0100

    added kahoot joiner

---
+++ Scripts/kahoot.js
@@ -0,0 +1,33 @@
+const Kahoot = require('kahoot.js-updated')
+
+exports.kahoot = async function (message) {
+  name = ['Mike Oxlong', 'Nick Gurr', 'Coquimash Schwan-Slang', 'Sakma Deek']
+  var ID = message.body.split(' ')[1]
+  client = []
+  // kahootid = 9295122
+
+  for (let index = 0; index < name.length; index++) {
+    client[index] = new Kahoot()
+    console.log('Joining kahoot...')
+    client[index].join(ID /* Or any other kahoot game pin */, name[index])
+    client[index].on('Joined', () => {
+      console.log('I joined the Kahoot!')
+      gclient.sendText(message.from, 'Joined kahoot with ID ' + ID + ' as *' + name[index] + '*')
+    })
+    await Sleep(1000)
+    client[index].on('QuizStart', () => {
+      console.log('The quiz has started!')
+    })
+    client[index].on('QuestionStart', (question) => {
+      console.log('A new question has started, answering the first answer.')
+      question.answer(0)
+    })
+    client[index].on('QuizEnd', () => {
+      console.log('The quiz has ended.')
+    })
+  }
+
+  function Sleep(milliseconds) {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds))
+  }
+}
